4141f26afd577997635f9f81362dd98d06ee1d84
Tanmay <tanmaykul29122002@gmail.com>
2023-03-28 19:20:41 +0530

    Changed the UI of Intro Screen

--- app/src/main/java/org/tensorflow/lite/examples/objectdetection/SplashScreenActivity.kt
+++ app/src/main/java/org/tensorflow/lite/examples/objectdetection/SplashScreenActivity.kt
@@ -6,19 +6,40 @@ import android.os.Handler
 import android.content.Intent
 import android.widget.ImageView
 import android.view.animation.AnimationUtils
+import androidx.constraintlayout.motion.widget.MotionLayout
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        val backgroundImg : ImageView = findViewById(R.id.iv_logo)
-        val sideAnimation = AnimationUtils.loadAnimation(this,R.anim.slide)
-        backgroundImg.startAnimation(sideAnimation)
-        Handler().postDelayed({
-            startActivity(Intent(this,MainActivity::class.java))
-            finish()
-        },3000)
+        val motionLayout = findViewById<MotionLayout>(R.id.motionLayout)
+        motionLayout.addTransitionListener(object: MotionLayout.TransitionListener{
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+
+            }
+
+        })
+
+//        val backgroundImg : ImageView = findViewById(R.id.iv_logo)
+//        val sideAnimation = AnimationUtils.loadAnimation(this,R.anim.slide)
+//        backgroundImg.startAnimation(sideAnimation)
+//        Handler().postDelayed({
+//            startActivity(Intent(this,MainActivity::class.java))
+//            finish()
+//        },3000)
 
     }
 }
