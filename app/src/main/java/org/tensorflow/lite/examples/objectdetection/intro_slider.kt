6f4e3bfec723825e08b3dd96db4fa30355f67bb1
Tanmay <tanmaykul29122002@gmail.com>
2023-03-31 20:12:30 +0530

    added new activities before object detection.

--- app/src/main/java/org/tensorflow/lite/examples/objectdetection/intro_slider.kt
+++ app/src/main/java/org/tensorflow/lite/examples/objectdetection/intro_slider.kt
@@ -2,18 +2,26 @@ package org.tensorflow.lite.examples.objectdetection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 
 class intro_slider : AppCompatActivity() {
 
     private lateinit var onboardingItemsAdapter: OnboardingItemsAdapter
+    private lateinit var indicatorsConatiner: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro_slider)
         setOnBoardingItem()
+        setupIndicators()
+        setCurrentIndicator(0)
     }
 
     private fun setOnBoardingItem() {
@@ -39,13 +47,75 @@ class intro_slider : AppCompatActivity() {
         val onboardingViewPager = findViewById<ViewPager2>(R.id.onBoardingViewPager)
         onboardingViewPager.adapter = onboardingItemsAdapter
 
+        onboardingViewPager.registerOnPageChangeCallback(object :
+            ViewPager2.OnPageChangeCallback(){
+                override fun onPageSelected(position: Int){
+                    super.onPageSelected(position)
+                    setCurrentIndicator(position)
+                }
+            })
+            (onboardingViewPager.getChildAt(0) as RecyclerView).overScrollMode =
+                RecyclerView.OVER_SCROLL_NEVER
+            findViewById<ImageView>(R.id.imageNext).setOnClickListener {
+                if(onboardingViewPager.currentItem+1 < onboardingItemsAdapter.itemCount){
+                    onboardingViewPager.currentItem+=1
+                }
+                else{
+                    finish()
+                }
+            }
+
+
         findViewById<TextView>(R.id.textSkip).setOnClickListener{
             finish()
         }
         findViewById<Button>(R.id.buttonGetStarted).setOnClickListener {
             finish()
         }
+    }
 
+    private fun setupIndicators(){
+        indicatorsConatiner = findViewById(R.id.indicatorsConatiner)
+        val indicators = arrayOfNulls<ImageView>(onboardingItemsAdapter.itemCount)
+        val layoutParams: LinearLayout.LayoutParams =
+            LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+        layoutParams.setMargins(8,0,8,0)
+        for(i in indicators.indices){
+            indicators[i] = ImageView(applicationContext)
+            indicators[i]?.let{
+                it.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_inactive_background
+                    )
+                )
+                it.layoutParams = layoutParams
+                indicatorsConatiner.addView(it)
+            }
+        }
+    }
+
+    private fun setCurrentIndicator(position: Int){
+        val childCount = indicatorsConatiner.childCount
+        for(i in 0 until childCount){
+            val imageView = indicatorsConatiner.getChildAt(i) as ImageView
+            if(i == position){
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_active_background
+                    )
+                )
+            }
+            else{
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_inactive_background
+                    )
+                )
+            }
+        }
     }
 
 }
